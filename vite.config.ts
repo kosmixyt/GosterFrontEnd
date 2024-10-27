b129bb09dc5aa6b73ebc91c5f68054c4d65744cf
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2024-10-27 12:38:16 +0100

    vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
+import { VitePWA } from 'vite-plugin-pwa'
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(),     VitePWA({ registerType: 'autoUpdate' })],
   server: {
     port: 3000,
   },
